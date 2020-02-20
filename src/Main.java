52c526d162fdd66f3a3ed58aea40dd8ed615cadf
Tarasenko <wjatscheslav47@gmail.com>
2020-02-20 22:07:14 +0200

    Added output

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -6,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -16,23 +19,34 @@ import java.util.stream.Collectors;
 public class Main
 {
 
-  public static String FILE_NAME = "a_example.txt";
   public static int booksCount;
   public static int librariesCount;
   public static int days;
   public List<Book> booksToScan = new ArrayList<>();
-  //  public static String FILE_NAME = "b_read_on.txt";
-//  public static String FILE_NAME = "c_incunabula.txt";
-//  public static String FILE_NAME = "d_tough_choices.txt";
-//  public static String FILE_NAME = "e_so_many_books.txt";
-//  public static String FILE_NAME = "f_libraries_of_the_world.txt";
+
+  public static String FILE_NAME_A = "a_example";
+  public static String FILE_NAME_B = "b_read_on";
+  public static String FILE_NAME_C = "c_incunabula";
+  public static String FILE_NAME_D = "d_tough_choices";
+  public static String FILE_NAME_E = "e_so_many_books";
+  public static String FILE_NAME_F = "f_libraries_of_the_world";
+
+//  public static String FILE_NAME_OUT_A = "out_a_example.txt";
+//  public static String FILE_NAME_OUT_B = "out_b_read_on.txt";
+//  public static String FILE_NAME_OUT_C = "out_c_incunabula.txt";
+//  public static String FILE_NAME_OUT_D = "out_d_tough_choices.txt";
+//  public static String FILE_NAME_OUT_E = "out_e_so_many_books.txt";
+//  public static String FILE_NAME_OUT_F = "out_f_libraries_of_the_world.txt";
 
   public static void main(String[] args) throws IOException
   {
     Main main = new Main();
-    List<Library> libraries = main.readLibraries(FILE_NAME);
-    List<Library> processedLibraries = main.getProcessedLibraries(libraries);
-    main.printProcessedLibrariesToFile(processedLibraries);
+    Arrays.asList(FILE_NAME_A, FILE_NAME_B, FILE_NAME_C, FILE_NAME_D, FILE_NAME_E, FILE_NAME_F)
+        .forEach(file -> {
+          List<Library> libraries = main.readLibraries(file);
+          List<Library> processedLibraries = main.getProcessedLibraries(libraries);
+          main.printProcessedLibrariesToFile(processedLibraries, file);
+        });
   }
 
   public List<Library> getProcessedLibraries(List<Library> libraries)
@@ -53,11 +67,9 @@ public class Main
     return processedLibraries;
   }
 
-  private void printProcessedLibrariesToFile(List<Library> libraries) throws IOException
+  private void printProcessedLibrariesToFile(List<Library> libraries, String fileName)
   {
-    Path out = Paths.get("result.out");
-    if (!Files.exists(out))
-      Files.createFile(out);
+    Path out = Paths.get(fileName + ".out");
     try (BufferedWriter writer = Files
         .newBufferedWriter(out, StandardCharsets.US_ASCII, StandardOpenOption.CREATE,
             StandardOpenOption.TRUNCATE_EXISTING))
@@ -72,6 +84,9 @@ public class Main
             .collect(Collectors.joining(" "));
         writer.write("\n" + res);
       }
+    } catch (IOException e)
+    {
+      e.printStackTrace();
     }
   }
 
@@ -79,13 +94,22 @@ public class Main
   private List<Book> getProcessedBooks(Library library, int remainingDays)
   {
     int isAbleToScan = remainingDays * library.booksPerDay;
+    if (isAbleToScan < 0)
+    {
+      isAbleToScan = Integer.MAX_VALUE;
+    }
+    if (isAbleToScan > library.books.size())
+    {
+      isAbleToScan = library.books.size();
+    }
+
     return library.books.subList(0, isAbleToScan);
   }
 
-  public List<Library> readLibraries(String fileName) throws IOException
+  public List<Library> readLibraries(String fileName)
   {
     List<Library> libraries = new ArrayList<>();
-    try (Scanner scanner = new Scanner(Paths.get(fileName)))
+    try (Scanner scanner = new Scanner(Paths.get(fileName + ".txt")))
     {
       Map<Integer, Book> booksById = new HashMap<>();
 
@@ -106,13 +130,15 @@ public class Main
           library.books.add(booksById.get(bookId));
         }
         library.score = calculateScore(library);
-        library.books.sort(Comparator.comparingInt(b -> b.score));
+        library.books.sort(Comparator.comparingInt(b -> ((Book) b).score).reversed());
         libraries.add(library);
       }
-      libraries.sort(Comparator.comparingDouble(l -> l.score));
+      libraries.sort(Comparator.comparingDouble(l -> ((Library) l).score).reversed());
       booksToScan.addAll(booksById.values());
-      booksToScan.sort(Comparator.comparingInt(b -> b.score));
+      booksToScan.sort(Comparator.comparingInt(b -> ((Book) b).score).reversed());
     }
+    catch (Exception ex)
+    {}
 
     return libraries;
   }
@@ -120,7 +146,10 @@ public class Main
   public int calculateScore(Library library)
   {
 //    return (days - library.signup) / (library.booksPerDay * calculateTotalScore(library));
-    return calculateTotalScore(library) / (library.signup + (library.booksCount / library.booksPerDay));
+
+//    return calculateTotalScore(library) / (library.signup + (library.booksCount / library.booksPerDay));
+
+    return (days- library.signup)/((library.booksCount/ library.booksPerDay)*(library.booksCount/calculateTotalScore(library)));
   }
 
   public int calculateTotalScore(Library library)
